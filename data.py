9483f7bb9273886bc12f703b15c810fd2969bbb8
Thomas Stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-01-09 15:19:07 +0000

    Changed the search results to sort games by number of ratings

--- data.py
+++ data.py
@@ -3,10 +3,10 @@ import pandas as pd
 def search_results(user_search):
     board_game_df = pd.read_csv("boardgames_ranks.csv")
     search_results = board_game_df[board_game_df["name"].str.contains(user_search, case=False)]
-    return pd.DataFrame(search_results["name"])
+    return search_results.sort_values(by="usersrated", ascending=False)
 
 def main():
-    search_results("Pandemic")
+    print(search_results("Pandemic"))
 
 if __name__ == "__main__":
     main()
