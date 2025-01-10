4bc1138a788ac2f22d30a06a1bb8944ac40396cd
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-01-10 18:45:22 +0000

    Created a new form class for rating games

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, HiddenField
+from wtforms import StringField, SubmitField, HiddenField, RadioField
 from wtforms.validators import InputRequired
 
 class SearchGame(FlaskForm):
@@ -9,3 +9,8 @@ class SearchGame(FlaskForm):
 class PickGame(FlaskForm):
     game_name = HiddenField("Game Name")
     add_game = SubmitField()
+
+
+class RateGame(FlaskForm):
+    ratings = RadioField("Rating", choices=[(2, "Strongly Like"), (1, "Like"), (0, "Neutral"), (-1, "Dislike"), (-2, "Strongly Dislike")], validators=[InputRequired()])
+    submit = SubmitField("Submit")
