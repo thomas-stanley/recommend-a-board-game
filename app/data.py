1c1c2bfe20c57e6d715fe729c0573ea7b45a56a4
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-01-15 17:46:17 +0000

    Fixed relative url of csv files so that they work when the code is run from root

--- app/data.py
+++ app/data.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 def search_results(user_search):
-    board_game_df = pd.read_csv("../data/boardgames_ranks.csv")
+    board_game_df = pd.read_csv("data/boardgames_ranks.csv")
     search_results = board_game_df[board_game_df["name"].str.contains(user_search, case=False)]
     return search_results.sort_values(by="usersrated", ascending=False)
 
 def find_id(game_name):
-    board_game_df = pd.read_csv("../data/boardgames_ranks.csv")
+    board_game_df = pd.read_csv("data/boardgames_ranks.csv")
     game_id = board_game_df[board_game_df["name"] == game_name]["id"]
     return game_id.iloc[0]
 
