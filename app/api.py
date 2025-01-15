a94bd5fad56e553d584562c7fae41941751d4716
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-01-15 20:30:20 +0000

    Fetching more games in a single request to drastically improve performance

--- app/api.py
+++ app/api.py
@@ -21,30 +21,44 @@ def game_details(game_id):
 
 def recommend_games(games_to_check, weighted_mechanics, user_games):
     recommended_games = []
+    to_search = []
+    counter = 0  # Counter for splitting the searches into twenties due to api max limit
+    search_ids = ""
     for game in games_to_check.iterrows():
-        if game[1]["name"] not in user_games:
-            url = f"https://www.boardgamegeek.com/xmlapi2/thing?id={game[1]["id"]}"
-            response = requests.get(url)
-            if response.status_code != 200:
-                print("Error fetching data")
-                continue
-            else:
-                tree = ET.fromstring(response.content)
-                board_game = tree.find("item")
-                mechanics = []
-                for link in board_game.findall("link"):
-                    if link.get("type") == "boardgamemechanic":
-                        game_mechanic = link.get("value")
-                        if game_mechanic in weighted_mechanics:
-                            mechanics.append(game_mechanic)
-                score = 0
-                for mechanic in mechanics:
-                    score += weighted_mechanics[mechanic]
-                print(f"Game: {game[1]['name']}, Score: {score}")
-                if score > 2:  # Minimum score to be recommended
-                    recommended_games.append((game[1]["name"], score))
-            sleep(0.5)
+        search_ids += str(game[1]["id"]) + ","
+        counter += 1
+        if counter % 20 == 0 or counter == len(games_to_check):
+            search_ids = search_ids[:-1]
+            to_search.append(search_ids)
+            search_ids = ""
+
+    for search in to_search:
+        url = f"https://www.boardgamegeek.com/xmlapi2/thing?id={search}"
+        response = requests.get(url)
+        if response.status_code != 200:
+            print("Error fetching data")
+            continue
+        else:
+            tree = ET.fromstring(response.content)
+            all_board_games = tree.findall("item")
+            for board_game in all_board_games:
+                game_name = board_game.find("name").get("value")
+                if game_name not in user_games:
+                    mechanics = []
+                    for link in board_game.findall("link"):
+                        if link.get("type") == "boardgamemechanic":
+                            game_mechanic = link.get("value")
+                            if game_mechanic in weighted_mechanics:
+                                mechanics.append(game_mechanic)
+                    score = 0
+                    for mechanic in mechanics:
+                        score += weighted_mechanics[mechanic]
+                    print(f"Game: {game_name}, Score: {score}")
+                    if score > 5:  # Minimum score to be recommended
+                        recommended_games.append((game_name, score))
+        sleep(1)
     sorted_games = sorted(recommended_games, key=lambda x: x[1], reverse=True)
+    print(sorted_games)
     return sorted_games
 
 
