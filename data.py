2129338767e835ab1b6916babead75d14c78bce4
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-01-15 16:58:52 +0000

    Added function to find the game id of a specific name

--- data.py
+++ data.py
@@ -5,8 +5,15 @@ def search_results(user_search):
     search_results = board_game_df[board_game_df["name"].str.contains(user_search, case=False)]
     return search_results.sort_values(by="usersrated", ascending=False)
 
+def find_id(game_name):
+    board_game_df = pd.read_csv("boardgames_ranks.csv")
+    game_id = board_game_df[board_game_df["name"] == game_name]["id"]
+    return game_id.iloc[0]
+
+
 def main():
     print(search_results("Pandemic"))
+    print(find_id("Pandemic"))
 
 if __name__ == "__main__":
     main()
