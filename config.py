1eefa373e97d55809f1e6008fba3370b587e7266
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-01-18 19:26:14 +0000

    Working on it

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:  # Base config class for all environments
+
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    BGG_API_ENDPOINT = "https://www.boardgamegeek.com/xmlapi2/"
+    BGG_API_DELAY = 1  # Currently unsure of the rate limit, so I'm keeping it at 1 for now because I know this works with low traffic
+
+    DEBUG = False
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    pass
+
+class TestingConfig(Config):
+    TESTING = True
+
+config = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "testing": TestingConfig,
+    "default": DevelopmentConfig
+}
