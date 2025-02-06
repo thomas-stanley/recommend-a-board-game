c476fb2d7eca30e38566d3694682f8af5f6e42f0
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-02-06 21:58:37 +0000

    File to remove excess columns

---
+++ cleanpandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv("data/boardgames_ranks.csv")
+
+# Drop columns
+df = df.drop(columns=["yearpublished", "bayesaverage", "average", "is_expansion", "abstracts_rank", "cgs_rank", "childrensgames_rank", "familygames_rank", "partygames_rank", "strategygames_rank", "thematic_rank", "wargames_rank"])
+df.to_csv("data/cleaned_boardgames_ranks.csv", index=False)
