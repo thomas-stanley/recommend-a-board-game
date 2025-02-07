14770aa7053b76cfffc32a4192a11e4484029fd7
Thomas Stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-02-07 15:57:52 +0000

    Added sql data

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ class Config:  # Base config class for all environments
 
     SECRET_KEY = os.environ.get("SECRET_KEY")
 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("sqlite:///", "sqlite:///"+os.path.abspath(os.getcwd())+"/")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     BGG_API_ENDPOINT = "https://www.boardgamegeek.com/xmlapi2/"
     BGG_API_DELAY = 1  # Currently unsure of the rate limit, so I'm keeping it at 1 for now because I know this works with low traffic
 
