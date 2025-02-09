69fd09a53c651d17a93e7225b7f83649f88ac101
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-02-09 22:21:03 +0000

    Now using the new schema stored in app.models.game

--- csvtosql.py
+++ csvtosql.py
@@ -1,25 +1,19 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
+from app.models.game import db, BoardGame
+import os
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data/boardgames.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL").replace("sqlite:///", "sqlite:///"+os.path.abspath(os.getcwd())+"/")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
-
-class BoardGame(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
-    name = db.Column(db.String(100), nullable=False)
-    rank = db.Column(db.Integer, nullable=False)
-    usersrated = db.Column(db.Integer, nullable=False)
+db.init_app(app)
 
 with app.app_context():
     db.create_all()
 
     df = pd.read_csv("data/cleaned_boardgames_ranks.csv")
-    print(df.columns)
 
     for bg_index, row in df.iterrows():
         board_game = BoardGame(
@@ -31,5 +25,4 @@ with app.app_context():
 
 
         db.session.add(board_game)
-    print(BoardGame.query.all())
     db.session.commit()
