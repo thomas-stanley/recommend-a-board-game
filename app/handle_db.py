abab770681a862b3c25b9508f9630c8f130df3e7
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-02-09 22:55:12 +0000

    Now no longer using the api and entirely using the database

--- app/handle_db.py
+++ app/handle_db.py
@@ -13,3 +13,18 @@ def find_id(game_name):
 def suitable_games():
     sorted_games = BoardGame.query.order_by(BoardGame.usersrated.desc()).limit(500).all()  # Change the limit to however many games to test for
     return tuple((game.id, game.name) for game in sorted_games)
+
+def game_details(game_id):
+    game = BoardGame.query.filter_by(id=game_id).first()
+    return game.mechanics
+
+def recommend_games(weighted_mechanics, user_games):
+    game_scores = []
+    games = BoardGame.query.all()
+    for game in games:
+        if game.name not in user_games:
+            total_score = sum(weighted_mechanics.get(mechanic, 0) for mechanic in game.mechanics)  # Sums all mechanics weights and adds 0 if no weight
+            game_scores.append((game.name, total_score))
+    sorted_scores = sorted(game_scores, key=lambda x: x[1], reverse=True)  # Sort by the largest score first
+    print(sorted_scores[:10])
+    return sorted_scores
