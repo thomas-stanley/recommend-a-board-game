8ff82090f0b790706c605602863560d3a2df834f
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-02-09 22:19:27 +0000

    Now removes games with few ratings

--- cleanpandas.py
+++ cleanpandas.py
@@ -4,4 +4,5 @@ df = pd.read_csv("data/boardgames_ranks.csv")
 
 # Drop columns
 df = df.drop(columns=["yearpublished", "bayesaverage", "average", "is_expansion", "abstracts_rank", "cgs_rank", "childrensgames_rank", "familygames_rank", "partygames_rank", "strategygames_rank", "thematic_rank", "wargames_rank"])
+df = df[df.usersrated >= 100]
 df.to_csv("data/cleaned_boardgames_ranks.csv", index=False)
