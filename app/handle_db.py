64e532caebd3b9efa8e84fea382fac89b551b79d
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-02-10 19:45:52 +0000

    No longer recommends expansions and weights games to favour higher rated ones

--- app/handle_db.py
+++ app/handle_db.py
@@ -14,8 +14,8 @@ def recommend_games(weighted_mechanics, user_games):
     game_scores = []
     games = BoardGame.query.all()
     for game in games:
-        if game.name not in user_games:
-            total_score = sum(weighted_mechanics.get(mechanic, 0) for mechanic in game.mechanics)  # Sums all mechanics weights and adds 0 if no weight
+        if game.name not in user_games and not game.is_expansion:  # Makes sure that the game is not in the user's list and is not an expansion
+            total_score = game.bayesaverage * (sum(weighted_mechanics.get(mechanic, 0) for mechanic in game.mechanics))  # Sums all mechanics weights and adds 0 if no weight, then multiplies by the bayesaverage to favour higher rated games
             game_scores.append((game.name, total_score))
     sorted_scores = sorted(game_scores, key=lambda x: x[1], reverse=True)  # Sort by the largest score first
     print(sorted_scores[:10])
