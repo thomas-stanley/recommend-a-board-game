00b227754b0cae92084202d7c2578c6b6bfdc892
Thomas Stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-02-13 15:04:27 +0000

    Changed generate env file path

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@ python3 -m venv venv
 pip install -r requirements.txt
 sqlite3 data/boardgames.db < data/boardgames_dump.sql
 sqlite3 data/boardgames.db "PRAGMA foreign_keys = ON;"
-python3 generate_env.py
+python3 scripts/generate_env.py
 python3 -m app.main
