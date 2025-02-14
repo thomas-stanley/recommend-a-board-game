2cb6dfb8beebd539154a8467aa6402b18da058ac
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-02-14 20:29:51 +0000

    Alter model to the new, umbrella version for many stats

--- app/models/game.py
+++ app/models/game.py
@@ -1,10 +1,10 @@
 from app.extensions import db
 
-class BoardGameMechanic(db.Model):
-    __tablename__ = "board_game_mechanics"
+class BoardGameFeature(db.Model):
+    __tablename__ = "board_game_features"
 
     board_game_id = db.Column(db.Integer, db.ForeignKey("board_game.id"), primary_key=True, nullable=False)
-    mechanic_id = db.Column(db.Integer, db.ForeignKey("mechanic.id"), primary_key=True, nullable=False)
+    mechanic_id = db.Column(db.Integer, db.ForeignKey("feature.id"), primary_key=True, nullable=False)
 
 
 class BoardGame(db.Model):
@@ -15,20 +15,20 @@ class BoardGame(db.Model):
     bayesaverage = db.Column(db.Float, nullable=False)
     usersrated = db.Column(db.Integer, nullable=False)
     is_expansion = db.Column(db.Boolean, nullable=False)
-    mechanics = db.relationship(
-        "Mechanic", # Related model
-        secondary="board_game_mechanics", # Association table
+    features = db.relationship(
+        "Feature", # Related model
+        secondary="board_game_features", # Association table
         back_populates="board_games" # Name of relationship in related model
     )
 
-class Mechanic(db.Model):
-    __tablename__ = "mechanic"
+class Feature(db.Model):
+    __tablename__ = "feature"
 
     id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
     board_games = db.relationship(
         "BoardGame", # Related model
-        secondary="board_game_mechanics", # Association table
-        back_populates="mechanics" # Name of relationship in related model
+        secondary="board_game_features", # Association table
+        back_populates="features" # Name of relationship in related model
     )
 
